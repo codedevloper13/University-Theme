17c4e261df5a66be15a990c8848db8e1b81b23f7
codedevloper13 <54003879+codedevloper13@users.noreply.github.com>
2020-08-17 23:16:35 +0530

    create past event arcive list

--- functions.php
+++ functions.php
@@ -143,3 +143,33 @@ function university_adjust_query($query)
 }
 
 add_action('pre_get_posts', 'university_adjust_query');
+
+// After theme activate automatically past-event page Created...
+
+function university_page_on_theme_activation(){
+
+    // Set the title, template, etc
+    $new_page_title     = __('Past Events','university'); // Page's title
+    $new_page_content   = '';                           // Content goes here
+    $new_page_template  = 'page-past-events.php';       // The template to use for the page
+    $page_check = get_page_by_title($new_page_title);   // Check if the page already exists
+    // Store the above data in an array
+    $new_page = array(
+        'post_type'     => 'page',
+        'post_title'    => $new_page_title,
+        'post_content'  => $new_page_content,
+        'post_status'   => 'publish',
+        'post_author'   => 1,
+        'post_name'     => 'past-events'
+    );
+    // If the page doesn't already exist, create it
+    if(!isset($page_check->ID)){
+        $new_page_id = wp_insert_post($new_page);
+        if(!empty($new_page_template)){
+            update_post_meta($new_page_id, '_wp_page_template', $new_page_template);
+        }
+    }
+}
+
+add_action( 'after_switch_theme', 'university_page_on_theme_activation' );
+
